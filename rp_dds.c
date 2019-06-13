8db615c22db6f63fb639d0aa5e5d8d09d05e4e78
Sakama Shunsuke <sakama@iis.u-tokyo.ac.jp>
2019-06-13 10:34:49 +0000

    Added option to change frequency and amplitude using binary value

--- rp_dds.c
+++ rp_dds.c
@@ -45,18 +45,23 @@ int rp_dds_open(RP_DDS * rp_dds, off_t hardware_addr, int sys_clk_mhz)
 int rp_dds_set_freq(RP_DDS * rp_dds, double freq0, double freq1)
 {
   int sys_clk = rp_dds->sys_clk_mhz;
-  volatile uint32_t *pin = ((uint32_t*) ((rp_dds->dds_cfg) + 0));
   int pWidth = 1 << PHASE_WIDTH;
   
   uint32_t pin0 = (uint32_t) (freq0 * (double) (pWidth / sys_clk));
   uint32_t pin1 = (uint32_t) (freq1 * (double) (pWidth / sys_clk));
+  
+  return rp_dds_set_freq_bin(rp_dds, pin0, pin1);
+}
 
+int rp_dds_set_freq_bin(RP_DDS * rp_dds, uint32_t pin0, uint32_t pin1)
+{
+  volatile uint32_t *pin = ((uint32_t*) ((rp_dds->dds_cfg) + 0));
   rp_dds->pin0 = pin0;
   rp_dds->pin1 = pin1;
 
   *(pin + 0) = pin0;
   *(pin + 1) = pin1;
-  
+
   return EXIT_SUCCESS;
 }
 
@@ -68,12 +73,16 @@ int rp_dds_set_ampl(RP_DDS * rp_dds, double ampl0, double ampl1)
   uint16_t asf0 = (uint16_t) (ampl0 * (1 << 9)) & 0x3FF;
   uint16_t asf1 = (uint16_t) (ampl1 * (1 << 9)) & 0x3FF;
 
+  return rp_dds_set_ampl_bin(rp_dds, asf0, asf1);
+}
+
+int rp_dds_set_ampl_bin(RP_DDS * rp_dds, uint16_t asf0, uint32_t asf1)
+{
   uint32_t * asf = ((uint32_t*) ((rp_dds->dds_cfg) + 8));
   *(asf) = (asf0 | (asf1 << 10)) & 0xFFFFFFFF;
-
   rp_dds->asf0 = asf0;
   rp_dds->asf1 = asf1;
-  
+
   return EXIT_SUCCESS;
 }
 
--- rp_dds.h
+++ rp_dds.h
@@ -1,6 +1,7 @@
 #ifndef __RP_DDS_H__
 #define __RP_DDS_H__
 
+#include <stdint.h>
 #include <sys/types.h>
 
 #define PHASE_WIDTH 30
@@ -18,7 +19,9 @@ typedef struct
 
 int rp_dds_open(RP_DDS * rp_dds, off_t hardware_addr, int sys_clk_mhz);
 int rp_dds_set_freq(RP_DDS * rp_dds, double freq0, double freq1);
-int rp_dds_set_ampl(RP_DDS * rp_dds, double ampl0b, double ampl1);
+int rp_dds_set_freq_bin(RP_DDS * rp_dds, uint32_t pin0, uint32_t pin1);
+int rp_dds_set_ampl(RP_DDS * rp_dds, double ampl0, double ampl1);
+int rp_dds_set_ampl_bin(RP_DDS * rp_dds, uint16_t asf0, uint32_t asf1);
 int rp_dds_close(RP_DDS * rp_dds);
 
 #endif
